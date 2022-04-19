c5ab47ba2faac9ae7fbf7057726b697d24ffce09
ivan77d <ivandrokin@gmail.com>
2022-04-19 11:09:08 +0300

    1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,23 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+        System.out.println("");
+
+        Cat bean2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean2.getMessage());
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean3.getMessage());
+        System.out.println("");
+
+        boolean hello = bean == bean1;
+        boolean cat = bean2 == bean3;
+
+        System.out.println(hello);
+        System.out.println(cat);
     }
 }
